339410215166e4bbf7bc07099c09ad4059ef7089
Benja Feuereisen <bgfeuereisen@uc.cl>
2020-05-08 21:54:52 -0400

    asd

--- db/migrate/20200508153201_addtousers.rb
+++ db/migrate/20200508153201_addtousers.rb
@@ -1,7 +1,7 @@
 class Addtousers < ActiveRecord::Migration[5.2]
   def change
-    #remove_column :meetings, :usuario2
-    remove_column :meetings, :rid
+    remove_column :meetings, :usuario2, :string
+    remove_column :meetings, :rid, :string
     add_column :meetings, :usuario2, :string
     add_column :meetings, :rid, :integer
 
