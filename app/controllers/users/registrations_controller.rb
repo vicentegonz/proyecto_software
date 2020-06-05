ce23d4ced2a16915ac7be581ad7a0c9bb28fbc2c
Benja Feuereisen <bgfeuereisen@uc.cl>
2020-06-05 15:00:22 -0400

    error heroku

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,7 +42,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :tipo, :sexo, :nombre, :apellidos, :contacto, :comuna_id, :edad, :descripcion, :foto])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :tipo, :sexo, :nombre, :apellidos, :contacto, :comuna, :edad, :descripcion, :foto])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
