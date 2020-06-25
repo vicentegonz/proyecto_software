017036848e90d5fea2255e7748f256ae4bec7efb
Pascual <you@example.com>
2020-06-24 22:01:24 -0400

    Listo el diseño

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -21,7 +21,7 @@ class RestaurantsController < ApplicationController
   
 
   def create
-    restaurant_params = params.require(:restaurant).permit(:nombre, :valoracion, :comentar, :descripcion, :aceptado, :foto, :address)
+    restaurant_params = params.require(:restaurant).permit(:nombre, :valoracion, :comentar, :descripcion, :aceptado, :foto, :address, :especialidad)
     @restaurant = Restaurant.new(restaurant_params.merge(user_id: current_user.id, comuna_id: @comuna.id))
     @restaurant.comuna = @comuna
     respond_to do |format|
@@ -35,7 +35,7 @@ class RestaurantsController < ApplicationController
   end
 
   def update
-    restaurant_params = params.require(:restaurant).permit(:nombre, :valoracion, :comentar, :descripcion, :aceptado, :foto, :address)
+    restaurant_params = params.require(:restaurant).permit(:nombre, :valoracion, :comentar, :descripcion, :aceptado, :foto, :address, :especialidad)
     @restaurant = Restaurant.find(params[:id])
     if @restaurant.update(restaurant_params)
       redirect_to restauran_path(@restaurant.id), notice:'Se ha creado correctamente'
