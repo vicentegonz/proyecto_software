65c3ee09256b8aae9fdba355e01b63b51102e688
Pascual <you@example.com>
2020-06-27 18:01:50 -0400

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,5 +101,5 @@ Rails.application.configure do
     user_name: ENV["GMAIL_USERNAME"],
     password:ENV["GMAIL_PASSWORD"]
   }
-  config.action_mailer.default_url_options = { :host => 'yourhostsite.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://halforangeinc.herokuapp.com' }
 end
